9362a8d33d33aee30ce51f10574e10dd56dec4c4
TheRockYT <jonasfelsen@gmail.com>
2023-09-15 09:14:08 +0200

    Added download settings to config

--- config.jl
+++ config.jl
@@ -1,4 +1,10 @@
-# The path to the data file
+# Do you want to download the data file automatically? This will make requests to external servers.
+config_download::Bool = false
+
+# Do you want to update to the latest data file every time you run the script? Requires: config_download = true
+config_update::Bool = false
+
+# The path to the data file. Not requried if: config_download = true
 config_data_file::String = "./Download-GDPcurrent-USD-countries.xlsx"
 
 # Enable logging of the result into the console
