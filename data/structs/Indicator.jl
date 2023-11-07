dcc4f00d4573d8429a12851116f0f3e0c1a54c6a
TheRockYT <jonasfelsen@gmail.com>
2023-11-07 09:53:48 +0100

    "Added getPeakYear and getBaseYear functions to Indicator struct"

--- data/structs/Indicator.jl
+++ data/structs/Indicator.jl
@@ -3,3 +3,25 @@ mutable struct Indicator
     name::String
     values::Dict{Int64,Float64}
 end
+function getPeakYear(indicator::Indicator)
+    peak_year = missing
+    peak_value = missing
+    for (year, value) in indicator.values
+        if ismissing(peak_year) || value > peak_value
+            peak_year = year
+            peak_value = value
+        end
+    end
+    return peak_year
+end
+function getBaseYear(indicator::Indicator)
+    base_year = missing
+    base_value = missing
+    for (year, value) in indicator.values
+        if ismissing(base_year) || value < base_value
+            base_year = year
+            base_value = value
+        end
+    end
+    return base_year
+end
