65e400a9e477a77cb44f08f8cfccc4e78edd385b
TheRockYT <91672537+TheRockYT@users.noreply.github.com>
2023-11-23 16:01:42 +0000

    Added status to 4.1.5

--- main.jl
+++ main.jl
@@ -13,7 +13,7 @@ mkdir("out")
 
 println("Saving results to output.xlsx...")
 # Write the results to a new excel file
-XLSX.openxlsx(config_output_file, mode = "w") do xf
+XLSX.openxlsx(config_output_file, mode="w") do xf
     # Add number format for the output
     num_fmt[] = XLSX.styles_add_numFmt(xf.workbook, string("#,##0"))
     # Use the default (first) sheet for a description.
@@ -43,8 +43,8 @@ XLSX.openxlsx(config_output_file, mode = "w") do xf
         # Get the number of years of data
         country_years = length(getIndicator(country, "Final consumption expenditure").values)
         # Add the country name
-        output_frequency_table[i + 1, 1] = country.name
-        output_frequency_table[i + 1, 2] = country_years
+        output_frequency_table[i+1, 1] = country.name
+        output_frequency_table[i+1, 2] = country_years
         # Check if the country has data for the entire period
         if country_years >= years_of_data
             frequency_table_countries_with_data += 1
@@ -52,7 +52,7 @@ XLSX.openxlsx(config_output_file, mode = "w") do xf
     end
     # Add number of countries with data over the entire period
     output_frequency_table[1, 4] = "Number of countries with data for the entire period"
-    output_frequency_table[2, 4] = string(frequency_table_countries_with_data, " / ", countries_of_data, " (", round(frequency_table_countries_with_data / countries_of_data * 100, digits = 2), "%)")
+    output_frequency_table[2, 4] = string(frequency_table_countries_with_data, " / ", countries_of_data, " (", round(frequency_table_countries_with_data / countries_of_data * 100, digits=2), "%)")
 
     # 4.1.2
     output_net_exports = XLSX.addsheet!(xf, "net_exports")
@@ -60,10 +60,10 @@ XLSX.openxlsx(config_output_file, mode = "w") do xf
     output_net_exports[2, 1] = "Country"
     # Save the years in the second row
     for (i, year) in enumerate(years)
-        output_net_exports[2, i + 1] = year
+        output_net_exports[2, i+1] = year
     end
     for (i, country) in enumerate(countries)
-        output_net_exports[i + 2, 1] = country.name
+        output_net_exports[i+2, 1] = country.name
         country_imports = getIndicator(country, "Imports of goods and services")
         country_exports = getIndicator(country, "Exports of goods and services")
         if !ismissing(country_imports) && !ismissing(country_exports)
@@ -71,7 +71,7 @@ XLSX.openxlsx(config_output_file, mode = "w") do xf
             country_exports_value = country_exports.values
             for (j, year) in enumerate(years)
                 if haskey(country_imports_value, year) && haskey(country_exports_value, year)
-                    output_net_exports[i + 2, j + 1] = country_exports_value[year] - country_imports_value[year]
+                    output_net_exports[i+2, j+1] = country_exports_value[year] - country_imports_value[year]
                 end
             end
         else
@@ -88,16 +88,16 @@ XLSX.openxlsx(config_output_file, mode = "w") do xf
     output_components_of_gdp[2, 2] = "Indicator"
     # Save the years in the second row
     for (i, year) in enumerate(years)
-        output_components_of_gdp[2, i + 2] = year
+        output_components_of_gdp[2, i+2] = year
     end
-     for (i, country) in enumerate(countries)
+    for (i, country) in enumerate(countries)
         current_start = i + 2 + (i - 1) * 3
         output_components_of_gdp[current_start, 1] = country.name
 
-        output_components_of_gdp[current_start + 0, 2] = "Household consumption expenditure (including Non-profit institutions serving households)"
-        output_components_of_gdp[current_start + 1, 2] = "General government final consumption expenditure"
-        output_components_of_gdp[current_start + 2, 2] = "Gross capital formation"
-        output_components_of_gdp[current_start + 3, 2] = "Net exports"
+        output_components_of_gdp[current_start+0, 2] = "Household consumption expenditure (including Non-profit institutions serving households)"
+        output_components_of_gdp[current_start+1, 2] = "General government final consumption expenditure"
+        output_components_of_gdp[current_start+2, 2] = "Gross capital formation"
+        output_components_of_gdp[current_start+3, 2] = "Net exports"
 
         household_consumption_expenditure = getIndicator(country, "Household consumption expenditure (including Non-profit institutions serving households)")
         general_government_final_expenditure = getIndicator(country, "General government final consumption expenditure")
@@ -113,16 +113,16 @@ XLSX.openxlsx(config_output_file, mode = "w") do xf
             country_exports_value = country_exports.values
             for (j, year) in enumerate(years)
                 if haskey(household_consumption_expenditure_value, year)
-                    output_components_of_gdp[current_start + 0, j + 2] = household_consumption_expenditure_value[year]
+                    output_components_of_gdp[current_start+0, j+2] = household_consumption_expenditure_value[year]
                 end
                 if haskey(general_government_final_expenditure_value, year)
-                    output_components_of_gdp[current_start + 1, j + 2] = general_government_final_expenditure_value[year]
+                    output_components_of_gdp[current_start+1, j+2] = general_government_final_expenditure_value[year]
                 end
                 if haskey(gross_capital_formation_value, year)
-                    output_components_of_gdp[current_start + 2, j + 2] = gross_capital_formation_value[year]
+                    output_components_of_gdp[current_start+2, j+2] = gross_capital_formation_value[year]
                 end
                 if haskey(country_imports_value, year) && haskey(country_exports_value, year)
-                    output_components_of_gdp[current_start + 3, j + 2] = country_exports_value[year] - country_imports_value[year]
+                    output_components_of_gdp[current_start+3, j+2] = country_exports_value[year] - country_imports_value[year]
                 end
             end
         else
@@ -146,13 +146,13 @@ XLSX.openxlsx(config_output_file, mode = "w") do xf
 
 
                 x = sort(collect(keys(household_consumption_expenditure.values)))
-                household_consumption_expenditure_key(key) = round(household_consumption_expenditure_value[key] / 1000000000, digits = 2)
-                general_government_final_expenditure_key(key) = round(general_government_final_expenditure_value[key] / 1000000000, digits = 2)
-                gross_capital_formation_key(key) = round(gross_capital_formation_value[key] / 1000000000, digits = 2)
-                net_exports_key(key) = round((country_exports_value[key] - country_imports_value[key]) / 1000000000, digits = 2)
+                household_consumption_expenditure_key(key) = round(household_consumption_expenditure_value[key] / 1000000000, digits=2)
+                general_government_final_expenditure_key(key) = round(general_government_final_expenditure_value[key] / 1000000000, digits=2)
+                gross_capital_formation_key(key) = round(gross_capital_formation_value[key] / 1000000000, digits=2)
+                net_exports_key(key) = round((country_exports_value[key] - country_imports_value[key]) / 1000000000, digits=2)
 
                 plot(x, [household_consumption_expenditure_key, general_government_final_expenditure_key, gross_capital_formation_key, net_exports_key], label=["Household consumption expenditure" "General government final consumption expenditure" "Gross capital formation" "Net exports"], title=country.name, xlabel="value in billion", ylabel="year")
-                
+
 
                 println("Saving plot for ", country.name, "...")
                 savefig(string("out/", country.name, ".png"))
@@ -207,48 +207,56 @@ XLSX.openxlsx(config_output_file, mode = "w") do xf
     end
     output_gdp_for_2015_selected = XLSX.addsheet!(xf, "gdp_for_2015_selected")
     output_gdp_for_2015_selected[1, 1] = "components of GDP for 2015 for selected countries"
-    output_gdp_for_2015_selected[2, 1] = "Country"
-    output_gdp_for_2015_selected[2, 2] = "Household consumption expenditure (including Non-profit institutions serving households)"
-    output_gdp_for_2015_selected[2, 3] = "General government final consumption expenditure"
-    output_gdp_for_2015_selected[2, 4] = "Gross capital formation"
-    output_gdp_for_2015_selected[2, 5] = "Net exports"
-    n = 3
-    for (i, country) in enumerate(countries)
-        current_start = i + 2
-        current_name = country.name
-        if current_name == "Iceland" || current_name == "Finland" || current_name == "Netherlands" || current_name == "Ukraine" || current_name == "Kazakhstan" || current_name == "Belarus" || current_name == "Philippines"  || current_name == "Zimbabwe"  || current_name == "China"
-            output_gdp_for_2015_selected[n, 1] = current_name
-            household_consumption_expenditure = getIndicator(country, "Household consumption expenditure (including Non-profit institutions serving households)")
-            general_government_final_expenditure = getIndicator(country, "General government final consumption expenditure")
-            gross_capital_formation = getIndicator(country, "Gross capital formation")
-            country_imports = getIndicator(country, "Imports of goods and services")
-            country_exports = getIndicator(country, "Exports of goods and services")
+    output_gdp_for_2015_selected[2, 1] = "Status"
+    output_gdp_for_2015_selected[2, 2] = "Country"
+    output_gdp_for_2015_selected[2, 3] = "Household consumption expenditure (including Non-profit institutions serving households)"
+    output_gdp_for_2015_selected[2, 4] = "General government final consumption expenditure"
+    output_gdp_for_2015_selected[2, 5] = "Gross capital formation"
+    output_gdp_for_2015_selected[2, 6] = "Net exports"
+    function addToSelected(n, country)
+        output_gdp_for_2015_selected[n, 2] = country.name
+        household_consumption_expenditure = getIndicator(country, "Household consumption expenditure (including Non-profit institutions serving households)")
+        general_government_final_expenditure = getIndicator(country, "General government final consumption expenditure")
+        gross_capital_formation = getIndicator(country, "Gross capital formation")
+        country_imports = getIndicator(country, "Imports of goods and services")
+        country_exports = getIndicator(country, "Exports of goods and services")
 
-            if !ismissing(household_consumption_expenditure) && !ismissing(general_government_final_expenditure) && !ismissing(gross_capital_formation) && !ismissing(country_imports) && !ismissing(country_exports)
-                household_consumption_expenditure_value = household_consumption_expenditure.values
-                general_government_final_expenditure_value = general_government_final_expenditure.values
-                gross_capital_formation_value = gross_capital_formation.values
-                country_imports_value = country_imports.values
-                country_exports_value = country_exports.values
-                year = 2015
-                if haskey(household_consumption_expenditure_value, year)
-                    output_gdp_for_2015_selected[n, 2] = household_consumption_expenditure_value[year]
-                end
-                if haskey(general_government_final_expenditure_value, year)
-                    output_gdp_for_2015_selected[n, 3] = general_government_final_expenditure_value[year]
-                end
-                if haskey(gross_capital_formation_value, year)
-                    output_gdp_for_2015_selected[n, 4] = gross_capital_formation_value[year]
-                end
-                if haskey(country_imports_value, year) && haskey(country_exports_value, year)
-                    output_gdp_for_2015_selected[n, 5] = country_exports_value[year] - country_imports_value[year]
-                end
-            else
-                println("Error: ", current_name, " does not fulfill 4.1.5 (b).")
+        if !ismissing(household_consumption_expenditure) && !ismissing(general_government_final_expenditure) && !ismissing(gross_capital_formation) && !ismissing(country_imports) && !ismissing(country_exports)
+            household_consumption_expenditure_value = household_consumption_expenditure.values
+            general_government_final_expenditure_value = general_government_final_expenditure.values
+            gross_capital_formation_value = gross_capital_formation.values
+            country_imports_value = country_imports.values
+            country_exports_value = country_exports.values
+            year = 2015
+            if haskey(household_consumption_expenditure_value, year)
+                output_gdp_for_2015_selected[n, 3] = household_consumption_expenditure_value[year]
             end
-            n = n + 1
+            if haskey(general_government_final_expenditure_value, year)
+                output_gdp_for_2015_selected[n, 4] = general_government_final_expenditure_value[year]
+            end
+            if haskey(gross_capital_formation_value, year)
+                output_gdp_for_2015_selected[n, 5] = gross_capital_formation_value[year]
+            end
+            if haskey(country_imports_value, year) && haskey(country_exports_value, year)
+                output_gdp_for_2015_selected[n, 6] = country_exports_value[year] - country_imports_value[year]
+            end
+        else
+            println("Error: ", country.name, " does not fulfill 4.1.5 (b).")
         end
     end
+    n = 3
+    output_gdp_for_2015_selected[n+0, 1] = "Developed economies"
+    addToSelected(n + 0, getCountry(parsed_data, "Iceland"))
+    addToSelected(n + 1, getCountry(parsed_data, "Finland"))
+    addToSelected(n + 2, getCountry(parsed_data, "Netherlands"))
+    output_gdp_for_2015_selected[n+3, 1] = "Economies in transition"
+    addToSelected(n + 3, getCountry(parsed_data, "Ukraine"))
+    addToSelected(n + 4, getCountry(parsed_data, "Kazakhstan"))
+    addToSelected(n + 5, getCountry(parsed_data, "Belarus"))
+    output_gdp_for_2015_selected[n+3, 1] = "Developing economies"
+    addToSelected(n + 6, getCountry(parsed_data, "Philippines"))
+    addToSelected(n + 7, getCountry(parsed_data, "Zimbabwe"))
+    addToSelected(n + 8, getCountry(parsed_data, "China"))
 end
 
 println("Done! See ya later!")
